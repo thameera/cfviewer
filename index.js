4140c20081f7643ebcaf28fab2a67880bf763b65
Thameera Senanayaka <thameera123@gmail.com>
2016-08-07 16:00:42 +0530

    Basic express serve

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Load env variables from .env
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(8080, () => {
+  console.log('Listening on port 8080!');
+});
