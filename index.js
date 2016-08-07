87bc2f8e75049f42a3c5d5e1c2b5665246acb5a2
Thameera Senanayaka <thameera123@gmail.com>
2016-08-07 16:54:37 +0530

    Call API with inputs

--- index.js
+++ index.js
@@ -4,11 +4,24 @@
 require('dotenv').config();
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const twitter = require('./twitter');
 
 const app = express();
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/get', (req, res) => {
+  if (!req.body.tweetId || !req.body.usernames) {
+    return res.sendStatus(400);
+  }
+  const tweetId = req.body.tweetId;
+  const usernames = req.body.usernames.split(' ');
+  res.json({});
+});
 
 app.get('/', (req, res) => {
   res.sendfile('public/index.html');
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,15 @@
 $(function() {
-  console.log('hello');
+
+  $('#view').click(function() {
+    var tweetId = $('#tweet_id').val();
+    var usernames = $('#usernames').val();
+
+    $.post('/get', {
+      tweetId: tweetId,
+      usernames: usernames
+    }, function(data) {
+      console.log(data);
+    });
+  });
+
 });
