8e5fc10fa4e9ed1a3c2d8be8372f31c1392ed1cc
chanux <chanuka@thinkcube.com>
2016-08-18 23:48:42 +0530

    Uses a full blown regex to validate tweet url this keeps non status urls at bay (ex: profile urls) Also drops query params from tweet urls if there are any

--- index.js
+++ index.js
@@ -19,10 +19,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/api/get-tweets', (req, res) => {
-  if (!req.body.tweetUrl || !req.body.tweetUrl.startsWith('https://twitter.com')) {
-    return res.sendStatus(400);
+  const valid_tweet = /https\:\/\/twitter\.com\/[a-zA-Z0-9_]{2,15}\/status\/[0-9]+/;
+  if (!req.body.tweetUrl || !valid_tweet.test(req.body.tweetUrl)) {
+    return res.json({error:true, message: "We expect a valid twitter status URL"});
   }
-  const tweetUrl = req.body.tweetUrl;
+
+  let tweetUrl = req.body.tweetUrl;
+  // Drop query params in tweet url if there are any
+  if(tweetUrl.indexOf('?') > -1) tweetUrl = tweetUrl.substring(0, tweetUrl.indexOf('?'));
+
   let usernames = [];
   if (req.body.usernames.length) {
     usernames = req.body.usernames.split(' ');
