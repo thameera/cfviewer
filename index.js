230b67bd2bdfcdc8b7668f9b3ebc643c6af4e4ac
chanux <chanux@gmail.com>
2016-08-19 21:06:07 +0530

    Fix regex for prepend url (#17)
    * Bulletproofs prepend url feature by dropping full blown
    regex because regex for express path params are weird
    This lets in tweets by handles containing numbers and underscaores
    
    * Uses a full blown regex to validate tweet url
    this keeps non status urls at bay (ex: profile urls)
    Also drops query params from tweet urls if there are any

--- index.js
+++ index.js
@@ -19,10 +19,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/api/get-tweets', (req, res) => {
-  if (!req.body.tweetUrl || !req.body.tweetUrl.startsWith('https://twitter.com')) {
-    return res.sendStatus(400);
+  const valid_tweet = /https\:\/\/twitter\.com\/[a-zA-Z0-9_]{2,15}\/status\/[0-9]+/;
+  if (!req.body.tweetUrl || !valid_tweet.test(req.body.tweetUrl)) {
+    return res.json({error:true, message: "We expect a valid twitter status URL"});
   }
-  const tweetUrl = req.body.tweetUrl;
+
+  let tweetUrl = req.body.tweetUrl;
+  // Drop query params in tweet url if there are any
+  if(tweetUrl.indexOf('?') > -1) tweetUrl = tweetUrl.substring(0, tweetUrl.indexOf('?'));
+
   let usernames = [];
   if (req.body.usernames.length) {
     usernames = req.body.usernames.split(' ');
@@ -44,8 +49,9 @@ app.post('/api/get-tweets', (req, res) => {
     });
 });
 
-app.get('/:tweetUrl(https\:\/\/twitter\.com\/[a-z]{2,15}\/status\/[0-9]+)', (req, res) => {
-  res.redirect('/?tweeturl=' + req.params.tweetUrl);
+app.get('/:prot*', (req, res) => {
+  const tweetUrl = req.params.prot + req.params[0];
+  res.redirect('/?tweeturl=' + tweetUrl);
 });
 
 app.listen(process.env.APP_PORT, () => {
